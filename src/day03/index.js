49f1dbc5531fe164d2fa1347504410d51076a712
André Linde <thedevpunk@gmail.com>
2020-12-03 22:41:20 +0100

    second star of day 3

--- src/day03/index.js
+++ src/day03/index.js
@@ -22,9 +22,42 @@ const day03 = () => {
     }
 
     return treeCount;
-  }
+  };
+
+  const partTwo = () => {
+    const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+    let treesPerSlope = 0;
+    let treesMultiplied = 1;
+
+    for (let i = 0; i < slopes.length; i++) {
+      let [stepGap, rowGap] = slopes[i];
+      let position = 0;
+      console.log(stepGap, rowGap);
+
+      for (let j = 0; j < rows.length; j += rowGap) {
+        let row = rows[j];
+        console.log(row);
+
+        while (row.length <= position) {
+          row += row;
+        }
+
+        if (row.charAt(position) === '#') {
+          treesPerSlope += 1;
+        }
+
+        position += stepGap;
+      }
+
+      treesMultiplied = treesMultiplied * treesPerSlope;
+      treesPerSlope = 0;
+    }
+
+    return treesMultiplied;
+  };
 
   console.log('Part One: ', partOne());
+  console.log('Part Two: ', partTwo());
 }
 
 day03();
