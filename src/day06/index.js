a93c7104999e88fd3f07e65dda7be918b17ec875
andrelindemail <andrelindemail@gmail.com>
2020-12-08 20:43:23 +0100

    second star of day 6

--- src/day06/index.js
+++ src/day06/index.js
@@ -17,8 +17,35 @@ const day06 = () => {
         return yesCounter;
     }
 
-    console.log('Part One:', partOne());
+    const partTwo = () => {
+        let yesCounter = 0;
 
+        for (let i = 0; i < groups.length; i++) {
+            const group = groups[i].split(/\r?\n/);
+
+            const groupMembers = group.length;
+
+            const groupYes = group.join('').split('').sort();
+            const filtered = groupYes.filter((elem, idx, array) => array.indexOf(elem) === idx);
+
+            let count = 0;
+
+            for (let j = 0; j < filtered.length; j++) {
+                const letter = filtered[j];
+
+                count = groupYes.filter(e => e === letter).length
+                
+                if (count === groupMembers) {
+                    yesCounter++;
+                }
+                count = 0;
+            }
+        }
+        return yesCounter;
+    }
+
+    console.log('Part One:', partOne());
+    console.log('Part Two:', partTwo());
 }
 
 day06();
